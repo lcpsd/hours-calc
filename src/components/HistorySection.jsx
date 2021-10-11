524b96d7b954a0cf174160298c4d7d6f43e2ad5b
Lucas Cardoso <lucascardosopsd@gmail.com>
2021-10-11 20:51:35 -0300

    history section style

--- src/components/HistorySection.jsx
+++ src/components/HistorySection.jsx
@@ -3,10 +3,40 @@ import '../styles/historySection.scss'
 
 export function HistorySection() {
     
+    const operations=[
+        {
+            id:1,
+            operation:"00:01:00 * 10",
+            result: "00:10:00"
+        },
+        {
+            id:2,
+            operation:"00:01:00 * 10",
+            result: "00:10:00"
+        },
+        {
+            id:3,
+            operation:"00:01:00 * 10",
+            result: "00:10:00"
+        },
+        {
+            id:4,
+            operation:"00:01:00 * 10",
+            result: "00:10:00"
+        },
+    ]
+
     return (
         <div id="history-section">
             <h3>History</h3>
-            <HistoryCard/>
+            {
+                operations.map(({id ,operation, result}) => {
+                    return <HistoryCard key={id} operation={operation} 
+                    result={result}
+                    />
+                })
+            }
+            
         </div>
     )
 }

074fa77a96158b60389b39088622ec84aaef243d
Lucas Cardoso <lucascardosopsd@gmail.com>
2021-10-11 15:57:04 -0300

    basic keyboard style and strcutore

--- src/components/KeyboardRow.jsx
+++ src/components/KeyboardRow.jsx
@@ -1,10 +1,25 @@
 import '../styles/keyboardRow.scss'
 
-export function KeyboardRow() {
+export function KeyboardRow({symbols}) {
     
     return (
         <div className="keyboard-row">
-
+            {
+                Object.entries(symbols)
+                .map(([key, value]) =>{
+                    return(
+                        <div key={key} className={
+                            value === "=" ? "equal":
+                            value === "+" ? "addition":
+                            value === "-" ? "subtraction":
+                            value === "x" ? "multiply":
+                            value === "÷" ? "divider":
+                            value
+                            
+                        }>{value}</div>
+                    )
+                })
+            }
         </div>
     )
 }
--- src/components/KeyboardSection.jsx
+++ src/components/KeyboardSection.jsx
@@ -1,3 +1,5 @@
+import {KeyboardRow} from '../components/KeyboardRow'
+
 import '../styles/keyboardSection.scss'
 
 export function KeyboardSection() {
@@ -5,6 +7,33 @@ export function KeyboardSection() {
     return (
         <div id="keyboard-section">
 
+            <KeyboardRow symbols={{
+                1: 7,
+                2: 8,
+                3: 9,
+                4: "AC"
+            }}/>
+
+            <KeyboardRow symbols={{
+                1: 4,
+                2: 5,
+                3: 6,
+                4: "÷"
+            }}/>
+
+            <KeyboardRow symbols={{
+                1: 1,
+                2: 2,
+                3: 3,
+                4: "x"
+            }}/>
+
+            <KeyboardRow symbols={{
+                1: 0,
+                2: "=",
+                3: "+",
+                4: "-"
+            }}/>
         </div>
     )
 }

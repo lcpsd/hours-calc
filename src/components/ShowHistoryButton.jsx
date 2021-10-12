04d26a7e762388faf64290ec655327c44546c54f
Lucas Cardoso <lucascardosopsd@gmail.com>
2021-10-11 22:05:25 -0300

    set history visible on click in ShowHistoryButton

--- src/components/ShowHistoryButton.jsx
+++ src/components/ShowHistoryButton.jsx
@@ -1,11 +1,15 @@
-import arrow from '../public/arrow.png'
+import { useHistoryHook } from '../hooks/useHistoryHook'
+import history from '../public/history.png'
 import '../styles/showHistoryButton.scss'
 
 export function ShowHistoryButton() {
     
+    const {setMenuVisibility} = useHistoryHook()
+
     return (
-        <button className="show-history-button">
-            <img src={arrow} alt="" />
+        <button className="show-history-button" 
+        onClick={() => setMenuVisibility('visible')}>
+            <img src={history} alt="" />
         </button>
     )
 }
